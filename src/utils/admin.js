830625fc524a4ac64e1a867d216e3ea1b50526b8
tzawr <aliseyfiazadsa6@gmail.com>
2026-05-04 18:52:46 -0700

    Activate admin UID

--- src/utils/admin.js
+++ src/utils/admin.js
@@ -1,6 +1,6 @@
 // Add your Firebase Auth UID here to unlock /admin/users and admin-only writes.
 export const ADMIN_UIDS = [
-  // 'YOUR_ADMIN_UID',
+  'AjV7SA0tuzLOcizYBrbjH8Mo0fg1',
 ]
 
 export function isAdminUid(uid) {
